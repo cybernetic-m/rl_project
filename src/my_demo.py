29a57385ea95722d494e3c589917c74a4c7515f3
cybernetic-m <romano.massimo.uni@gmail.com>
2025-04-03 13:39:37 +0200

    my_demo camera fix

--- src/my_demo.py
+++ src/my_demo.py
@@ -14,22 +14,27 @@ sys.path.append(robosuite_path)
 
 from robocasa.environments import ALL_KITCHEN_ENVIRONMENTS
 from robocasa.environments.kitchen.kitchen import Kitchen
+#from robocasa.environments.kitchen.multi_stage.washing_dishes.dry_dishes import dry_dishes as DryDishes
 from robocasa.utils.env_utils import create_env
 import numpy as np
 import imageio
+import matplotlib.pyplot as plt
 
+# You can put True/False for video_saving flag to save or not a video of your simulation
 if sys.platform.startswith("win"):
     print("\nYou are on Windows!\n")
     video_saving = False # Not saving video
 elif sys.platform == "darwin":
     print("\nYou are on MacOS\n")
-    video_saving = True
+    video_saving = True  # For M1 systems tested and works!
 elif sys.platform == "linux":
     print("\nYou are on Linux\n")
-    video_saving = True # For Linux systems (To test?)
+    video_saving = True # For Linux systems tested and works!
 
 # choose random task
+print (list(ALL_KITCHEN_ENVIRONMENTS))
 env_name = np.random.choice(list(ALL_KITCHEN_ENVIRONMENTS))  # name of the kitchen env
+
 controller_config = {
     "type": "BASIC",
     "input_max": 1.0,
@@ -42,6 +47,7 @@ controller_config = {
     "interpolation": None,
     "policy_freq": 20,
 }
+
 # set the camera parameters
 renderer_configuration={
     "lookat":[1.2,0.8,1.0],
@@ -52,16 +58,17 @@ env=Kitchen(
     robots="GR1FloatingBody",
     #robots="GR1",
     #controller_configs=controller_config,
-    has_renderer=True,
-    has_offscreen_renderer=False,
-    use_camera_obs=False,
+    has_renderer=True, 
+    has_offscreen_renderer=True,
+    use_camera_obs=True,
     render_camera=None,
-    control_freq=20,
-    renderer_config=renderer_configuration,
+    #control_freq=20,
+    #renderer_config=renderer_configuration,
     seed=3,
     init_robot_base_pos="sink", #where to start the robot
     layout_ids=-3, #island in the kitchen
     style_ids=9,#color and style on the kithen
+    camera_names=["robot0_robotview"], # Camera for the camera_obs
 )
 # reset the environment
 env.reset()
@@ -84,16 +91,19 @@ for i in range(500):
     if i == 0:
         for key in obs:
             print(key)
+    obs['robot0_robotview_image']
     env.render()  # render on display
     
     if video_saving:
         frame = env.sim.render(
-            camera_name = "robot0_eye_in_hand",
+            camera_name = "robot0_robotview",
             width=640, height=480,
             depth=False
         )
         video_writer.append_data(frame)
 
+# Another loop for saving a video
 if video_saving:
+    print("Saving the video")
     video_writer.close()
     print("Video saved as trial.mp4")
